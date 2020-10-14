1e34e6f0078eadd8f015aac1b692657fa0b79ab3
ashenicas <ashenicas@gmail.com>
2020-10-14 21:59:01 +0530

    google sentiment,translate and entity services

---
+++ back-end/main.py
@@ -0,0 +1,18 @@
+from .google_entities_helper import GoogleEntitiesHelper
+from .google_translate_helper import TranslateHelper
+from .sentiment_analysis_global_helper import SentimentAnalysisGlobalHelper
+import pandas as pd
+
+_sentiment_analysis_global_helper = SentimentAnalysisGlobalHelper()
+_google_translate_helper = TranslateHelper()
+_google_entities_helper = GoogleEntitiesHelper()
+
+df = pd.read_csv("../data/kingsbury-tripadvisor.csv")
+df = df.dropna(subset=['review'])
+
+df = _google_translate_helper.runner(df, "review")
+df = _google_entities_helper.runner(df, "translated")
+df = _sentiment_analysis_global_helper.runner(df, "translated")
+
+df.to_csv(r'kingsbury-tripadvisor-analized.csv', index=False)
+print(df)
